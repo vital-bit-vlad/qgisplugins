cb0e1d6ac49fcc90bd4fe3724aaab3f1c465cc1b
Vladyslav Gnatiuk <vgnatiuk@liquidnet.com>
2017-11-01 17:20:46 -0400

    TestPlugin1 created via QGIS Plugin Builder plugin

---
+++ TestPlugin1/TestPlugin1_dialog.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ TestPlugin1Dialog
+                                 A QGIS plugin
+ Test Plugin
+                             -------------------
+        begin                : 2017-11-01
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by vital-bit.com
+        email                : vlad@bital-bit.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'TestPlugin1_dialog_base.ui'))
+
+
+class TestPlugin1Dialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(TestPlugin1Dialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
---
+++ TestPlugin1/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ TestPlugin1
+                                 A QGIS plugin
+ Test Plugin
+                             -------------------
+        begin                : 2017-11-01
+        copyright            : (C) 2017 by vital-bit.com
+        email                : vlad@bital-bit.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load TestPlugin1 class from file TestPlugin1.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .TestPlugin1 import TestPlugin1
+    return TestPlugin1(iface)
